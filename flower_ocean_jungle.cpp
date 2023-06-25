63a99694bc2a99a0fa90a3dabef12db7b49e63d4
rhoksanasaeed1 <136628728+rhoksanasaeed1@users.noreply.github.com>
2023-06-25 15:13:20 +0700

    Update dependencies

---
+++ flower_ocean_jungle.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string joyOfLearning = "The Joy of Learning";
+    cout << joyOfLearning << endl;
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning can be such a joy when done with passion and enthusiasm!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning is a journey that can bring us closer to ourselves and to others!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning can open our minds to greater understanding!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning is a fulfilling experience that can bring us joy!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning can bring us closer to our goals and passions!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning can be fun, challenging, and rewarding!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning is an endless process with countless opportunities to gain knowledge!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning can give meaning and purpose to our lives!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Learning helps us to find our true potential!" << endl;
+    }
+    cout <<endl;
+ 
+    //This for-loop prints statements inspired by the joy of learning
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Through learning, we become more effective and knowledgeable!" << endl;
+    }
+    cout <<endl;
+ 
+    cout << "Experience the joy of learning!" << endl;
+ 
+    return 0;
+}
