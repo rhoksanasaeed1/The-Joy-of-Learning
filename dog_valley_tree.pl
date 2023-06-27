25421371538bd07045a9cd0ba9310f337d62d60f
rhoksanasaeed1 <136628728+rhoksanasaeed1@users.noreply.github.com>
2023-06-27 12:17:26 +0700

    Refactor variable names

---
+++ dog_valley_tree.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+# This program prints out the phrase 'The Joy of Learning'
+
+use strict;
+use warnings;
+
+# Declare and initialize variables
+my $count = 0;
+
+# Print 2000 lines of 'The Joy of Learning'
+while ($count < 2000) {
+	print "The Joy of Learning\n";
+	$count++;
+}
