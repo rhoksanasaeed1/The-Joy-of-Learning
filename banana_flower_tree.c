c459c322a4b714dba8f9f959ccc2a24e4ca037dc
rhoksanasaeed1 <136628728+rhoksanasaeed1@users.noreply.github.com>
2023-07-19 17:58:04 +0700

    Enhance error handling

---
+++ banana_flower_tree.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Main function to be called 
+int main() 
+{ 
+    printf("The Joy of Learning \n\n"); 
+      
+    //Loop to print 2000 lines 
+    int i; 
+    for (i = 0; i < 2000; i++) 
+        printf("Line %d: Learning is a lifelong journey and a rewarding experience! \n", i + 1); 
+      
+    return 0; 
+}
